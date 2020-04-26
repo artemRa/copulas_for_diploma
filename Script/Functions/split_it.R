7cb0f64525ff2c6a46778708ecd7dd682d34a00a
artemRa <aaartem.ra@gmail.com>
2020-04-27 00:36:18 +0300

    Анализ на данных Lending Club

--- Script/Functions/split_it.R
+++ Script/Functions/split_it.R
@@ -1,7 +1,7 @@
 # Разбивка таблицы на бакеты
 # X - входой data.frame признаков
 split_it <- 
-  function(X, breaks) {
+  function(X, breaks, m = 5) {
     map2(X, breaks, ~ cut(.x, .y, labels = seq(m))) %>% 
       as_tibble() %>% 
       mutate_if(is.factor, as.integer)
