dae4147047733d900eeb97e13d17dbdde4306f5e
artemRa <aaartem.ra@gmail.com>
2020-05-04 18:49:18 +0300

    Почти последняя поставка

--- Script/Step3.R
+++ Script/Step3.R
@@ -8,10 +8,17 @@ library(lubridate)
 library(ggpubr)
 library(conflicted)
 library(gganimate)
+library(yardstick)
 
 
 source("./Script/Functions/make_risk_hypercube.R")
 source("./Script/Functions/split_it.R")
+
+
+# Конфликты функций
+conflict_prefer("filter", "dplyr", "base")
+conflict_prefer("filter", "dplyr", "stats")
+conflict_prefer("select", "dplyr", "MASS")
 
 Sys.setlocale("LC_TIME", "C")
 loan <- read_csv("./Data/lending-club-loan-data/loan.csv")
@@ -315,8 +322,8 @@ cop_dict <-
     5, "Frank copula",
     6, "Joe copula",
     10, "BB8 copula", 
-    13, "rotated Clayton copula (180 degrees; “survival Clayton”)",
-    14, "rotated Gumbel copula (180 degrees; “survival Gumbel”)",
+    13, "rotated Clayton copula \n (180 degrees; “survival Clayton”)",
+    14, "rotated Gumbel copula \n (180 degrees; “survival Gumbel”)",
     20, "rotated BB8 copula (180 degrees; “survival BB8”)",
     30, "rotated BB8 copula (90 degrees)",
     33, "rotated Clayton copula (270 degrees)" 
@@ -446,10 +453,6 @@ fact_risk <-
 plot(steps, fact_risk, type = 'l', col = 'red', ylim = c(0.1, 0.15))
 
 
-# Конфликты функций
-conflict_prefer("filter", "dplyr", "base")
-conflict_prefer("filter", "dplyr", "stats")
-conflict_prefer("select", "dplyr", "MASS")
 
 
 
@@ -520,8 +523,11 @@ hypercube_history_df %>%
 
 ggsave("./Output/lendin_club_risk_hyper_dyn.png", width = 24, height = 5, dpi = 240)
 
+# save.image(file = "./Output/my_work_space.RData")
 
-N2 <- 500
+
+
+N2 <- 100
 risk_dots <- list()
 
 for (i in num_mature[-1]) {
@@ -533,45 +539,247 @@ for (i in num_mature[-1]) {
   reverse_data <- 
     map(1:NCOL(U), ~ approxfun(U[,.x], X[,.x] %>% pull())(U2[,.x])) %>% 
     as.data.frame()
+  
   colnames(reverse_data) <- risk_drivers
   
-  risk_dots[[i]] <- 
-    split_it(reverse_data, breaks) %>% 
-    mutate(split_gr = rep(seq(N2), N)) %>% 
-    group_by(.dots = c(risk_drivers, "split_gr")) %>%
-    summarise(sim_cnt = n()) %>% 
-    ungroup() %>% 
-    left_join(
-      hypercube_data_list[[1]] %>% 
-        mutate(risk = bads2/cnt2) %>% 
-        select(risk_drivers, risk),
-      by = risk_drivers
-    ) %>% 
-    group_by(split_gr) %>% 
-    summarise(risk = sum(sim_cnt * risk, na.rm = T) / sum(sim_cnt, na.rm = T)) %>% 
-    pull()
+  risk_dots[[i]] <- list()
+  
+  for (j in 1:i) {
+    
+    merge_df <- 
+      hypercube_data_list[[j]] %>% 
+      mutate(risk = bads2/cnt2, cl = 1/group) %>% 
+      select(risk_drivers, risk, cl)
+    
+    risk_dots[[i]][[j]] <- 
+      split_it(reverse_data, breaks) %>% 
+      mutate(split_gr = rep(seq(N2), N)) %>% 
+      group_by(.dots = c(risk_drivers, "split_gr")) %>%
+      summarise(sim_cnt = n()) %>% 
+      ungroup() %>% 
+      left_join(
+        merge_df,
+        by = risk_drivers
+      ) %>% 
+      group_by(split_gr) %>% 
+      summarise(
+        risk = sum(sim_cnt * risk, na.rm = T) / sum(sim_cnt, na.rm = T),
+        cl = sum(sim_cnt * cl, na.rm = T) / sum(sim_cnt, na.rm = T)
+      )
+    
+  }
+  
+  print(i)
 }
-
-
-
-
-hist(risk_dots)
 
 
 fact_risk <- 
   risk_data1 %>% 
   filter(charged_mob == 24) %>% 
-  filter(issue_d >= '2013-01-01') %>% 
+  filter(issue_d > '2013-01-01') %>% 
   arrange(issue_d) %>% 
-  mutate(n = row_number() + 1) %>% 
+  mutate(id = row_number()) %>% 
   filter(!is.na(risk)) %>% 
-  select(issue_d, risk)
+  select(id, issue_d, risk)
+
+median_predict <- 
+  map_df(
+    num_mature,
+    function(k) 
+      risk_dots %>% map_dbl( ~ ifelse(is.null(.x) | NROW(.x) < k, NA, median(.x[[k]]$risk)))
+  ) %>% 
+  mutate(id = row_number()) %>% 
+  pivot_longer(names_to = "risk_base", values_to = "median", -id) %>% 
+  mutate_if(is.character, as.Date)
+
+cl_predict <- 
+  map_df(
+    num_mature,
+    function(k) 
+      risk_dots %>% map_dbl( ~ ifelse(is.null(.x) | NROW(.x) < k, NA, median(.x[[k]]$cl)))
+  ) %>% 
+  mutate(id = row_number()) %>% 
+  pivot_longer(names_to = "risk_base", values_to = "cl", -id) %>% 
+  mutate_if(is.character, as.Date)
+
+full_predict <- 
+  map_df(
+    num_mature,
+    function(k) 
+      risk_dots %>% map_dbl( ~ ifelse(is.null(.x) | NROW(.x) < k, NA, sd(.x[[k]]$risk)))
+  ) %>% 
+  mutate(id = row_number()) %>% 
+  pivot_longer(names_to = "risk_base", values_to = "sd", -id) %>% 
+  mutate_if(is.character, as.Date) %>% 
+  left_join(median_predict, by = c("risk_base", "id")) %>% 
+  left_join(cl_predict, by = c("risk_base", "id"))
 
 
+loan_clear_grade <- 
+  loan_clear %>% 
+  mutate(issue_d = ifelse(issue_d < '2013-01-01', as.Date('2013-01-01'), issue_d) %>% as.Date(origin="1970-01-01")) %>% 
+  mutate(grade = ifelse(grade %in% c("E", "F", "G"), "E+", grade) %>% as.factor()) %>% 
+  group_by(issue_d, grade) %>% 
+  summarise(cnt = n(), risk = sum(bads24)/n()) %>% 
+  ungroup()
 
-merge_data <- 
+grade_split_data <- loan_clear_grade %>% 
+  left_join(loan_clear_grade %>% select(issue_d, grade, risk), by = "grade", suffix = c("", "_base")) %>% 
+  mutate(bads_sint = risk_base * cnt, bads = risk * cnt) %>% 
+  group_by(issue_d, issue_d_base) %>% 
+  summarise(risk_pred = sum(bads_sint, na.rm = T) / sum(cnt, na.rm = T), 
+            risk_fact = sum(bads, na.rm = T) / sum(cnt, na.rm = T)) %>% 
+  filter(issue_d > issue_d_base) %>% 
+  arrange(issue_d, desc(issue_d_base)) %>% 
+  mutate(lagged = row_number()) %>% 
+  # filter(lagged == 1) %>% 
+  filter(lagged %in% c(6, 12, 24)) %>%
+  filter(issue_d_base >  '2013-01-01') %>% 
+  left_join(fact_risk %>% select(issue_d, id), by = "issue_d")
 
-sim_group_data <- 
-  split_reverse_data %>% 
+
+loan_clear_risk_drivers <- map2(raw_xtr_list_group,
+                                as.Date(names(raw_xtr_list_group)),
+                                ~ mutate(.x, issue_d = .y)) %>% 
+  map_df(cbind) %>% 
+  group_by(.dots = c(risk_drivers, "issue_d")) %>% 
+  summarise(risk = sum(bads, na.rm = T) / n(), cnt = n())
+
+group_by_risk_drivers <- loan_clear_risk_drivers %>% 
+  left_join(loan_clear_risk_drivers, 
+            by = risk_drivers, 
+            suffix = c("", "_base")) %>% 
+  filter(issue_d > issue_d_base) %>% 
+  group_by(issue_d, issue_d_base) %>% 
+  summarise(risk_pred = sum(risk_base * cnt, na.rm = T) / sum(cnt, na.rm = T), 
+            risk_fact = sum(risk * cnt, na.rm = T) / sum(cnt, na.rm = T)) %>% 
+  arrange(issue_d, desc(issue_d_base)) %>% 
+  mutate(lagged = row_number()) %>% 
+  filter(lagged %in% c(1, 6, 12, 24)) %>% 
+  left_join(fact_risk %>% select(issue_d, id), by = "issue_d")
   
-  left_join(merge_data, by = risk_drivers)
+
+
+
+
+full_predict %>% 
+  mutate(id = id - 1) %>%
+  filter(!is.na(median)) %>% 
+  arrange(id, desc(risk_base)) %>% 
+  group_by(id) %>% 
+  mutate(lagged = row_number()-2) %>% 
+  ungroup() %>% 
+  # filter(risk_base >  '2013-01-01') %>% 
+  # filter(lagged == 1) %>% 
+  filter(lagged %in% c(6, 12, 24)) %>% 
+  # arrange(lagged, id) %>% 
+  # print(n =  100)
+  ggplot(aes(x = id)) +
+  geom_ribbon(aes(ymin = median - sd, 
+                  ymax = median + sd, 
+                  fill = as.factor(lagged)), 
+              alpha = 0.7) +
+  geom_ribbon(aes(ymin = median - 3*sd, 
+                  ymax = median + 3*sd, 
+                  fill = as.factor(lagged)), 
+              alpha = 0.3) +
+  geom_line(aes(y = median, color = as.factor(lagged), linetype = "D-vine prediction")) +
+  geom_line(data = fact_risk, aes(x = id, y = risk)) + 
+  geom_line(data = grade_split_data, 
+            aes(x = id, y = risk_pred, color = as.factor(lagged), linetype = "grade split prediction"), 
+            size = 1.2) +
+  # geom_line(data = group_by_risk_drivers, aes(x = id, y = risk_pred, color = as.factor(lagged)), linetype = 2) +  
+  labs(x = NULL, y = 'risk level', color = 'lag', fill = 'lag') +
+  ylim(0.08, 0.15)
+
+
+ggsave("./Output/lending_club_median_predict.png", width = 12, height = 9)
+
+
+mod6 <- 
+  fact_risk %>% 
+  inner_join(grade_split_data %>% 
+              ungroup() %>% 
+              # filter(lagged == 6) %>% 
+              select(id, lagged, risk_pred),
+            by = "id") %>% 
+  inner_join(full_predict %>% 
+              mutate(id = id - 1) %>%
+              filter(!is.na(median)) %>% 
+              arrange(id, desc(risk_base)) %>% 
+              group_by(id) %>% 
+              mutate(lagged = row_number()-2) %>% 
+              ungroup() %>% 
+              select(id, lagged, median),
+            by = c("id", "lagged")) %>% 
+  pivot_longer(cols = c("risk_pred", "median"),
+               names_to = "prediction", 
+               values_to = "risk_level") %>% 
+  group_by(lagged, prediction)
+
+# Сравнение
+mod6 %>% yardstick::rmse(risk, risk_level, na.rm = T)
+mod6 %>% yardstick::mae(risk, risk_level, na.rm = T)
+mod6 %>% yardstick::mase(risk, risk_level, na.rm = T)
+
+
+conflict_prefer("lag", "dplyr", "stats")
+
+x <- runif(5)
+cbind(ahead = lead(x), x, behind = lag(x))
+
+cop_params <-
+  flexy_family_params %>%
+  select(id, cop, fam) %>% 
+  arrange(cop, id) %>% 
+  mutate(change = ifelse(fam != lag(fam), 1, 0) %>% as.factor())
+
+
+cop_dict2 <- 
+  tribble(
+    ~ fam,  ~ copula,
+    1, "Gaussian copula", 
+    2, "Student t copula\n(t-copula)",
+    3, "Clayton copula",
+    5, "Frank copula",
+    6, "Joe copula",
+    10, "BB8 copula", 
+    13, "rotated Clayton copula\n(180 degrees; “survival Clayton”)",
+    14, "rotated Gumbel copula\n(180 degrees; “survival Gumbel”)",
+    20, "rotated BB8 copula\n(180 degrees; “survival BB8”)",
+    30, "rotated BB8 copula\n(90 degrees)",
+    33, "rotated Clayton copula\n(270 degrees)" 
+  ) %>% mutate_at("copula", as.factor)
+
+
+for (i in 1:3) {
+  hypercube_history_df %>% 
+    left_join(fact_risk %>% select(id, issue_d), by = c('date' = 'issue_d')) %>% 
+    mutate_at("id", ~ coalesce(., 0L)) %>% 
+    group_by(.dots = risk_drivers) %>% 
+    arrange(id) %>%
+    mutate(risk_diff = risk / lead(risk) - 1) %>% 
+    # filter_at(risk_drivers, ~ . == 1L)
+    ungroup() %>% 
+    left_join(cop_params, by = "id") %>%
+    left_join(cop_dict2, by = "fam") %>% 
+    # filter(int_rate == 5L) %>%
+    mutate(int_rate_gr = ifelse(int_rate == 5L, 'worst', 'other')) %>% 
+    filter_at(c("risk_diff", "change"), ~ !is.na(.)) %>%
+    filter(cop == i) %>%
+    ggplot(aes(x = as.factor(change), y = risk_diff, fill = as.factor(change))) +
+    geom_jitter(alpha = 0.1, size = 2) +
+    geom_violin(draw_quantiles = c(0.1, 0.25, 0.5, 0.75, 0.9), size = 0.75, alpha = 0.7) +
+    facet_grid(cols = vars(copula), rows = vars(int_rate_gr)) +
+    theme(strip.text.x = element_text(size = 9, angle = 90)) +
+    labs(fill = 'change', y = 'risk level change', x = NULL) +
+    scale_y_continuous(labels = scales::percent, limits = c(-1, 1))
+  
+  ggsave(paste0("./Output/lendin_club_cop_risk_distrib", i, ".png"), width = 12, height = 9, dpi = 500)
+  
+}
+
+
+
+  
+  
